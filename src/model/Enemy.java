30a1fc096734f77bca0862be9217ea957001e3b6
Taite Nazifi <taitenazifi@gmail.com>
2018-03-28 14:27:20 -0700

    Setting up Enemy object; setting up tower object; setting up enumeration for the types of enemies and towers we may have.

---
+++ src/model/Enemy.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.awt.Point;
+
+import javafx.scene.image.Image;
+
+public class Enemy {
+
+	private Image enemyimg; //new Image("file:..", false);
+	private double health;
+	private Point pos;
+}
