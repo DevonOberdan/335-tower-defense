ea5c5760e07589155613234edd9296054bd72f66
Badr Badghaish <Badr@dhcp-10-134-234-193.uawifi.arizona.edu>
2018-04-05 21:29:37 -0700

    map

--- src/controller/TowerDefenseGUI.java
+++ src/controller/TowerDefenseGUI.java
@@ -1,15 +1,27 @@
 package controller;
 
+import java.util.Observer;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import model.Map;
+import view.GameView;
 
 public class TowerDefenseGUI extends Application{
 
+	
+	private Observer currentView; // currentView will hold the current view of the observed
+	private BorderPane pane; // pane will represent the scene
+	private Observer gameView; // game view of the map
+	private Map theGame;
 	public static void main (String [] args)
 	{
 		launch(args);
@@ -18,11 +30,11 @@ public class TowerDefenseGUI extends Application{
 	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
 		stage.setTitle("Tower Defense");
-		BorderPane window = new BorderPane();
-		Scene scene = new Scene (window, 500,500);
-		GridPane pane = new GridPane();
-		pane.setVgap(10);
-		pane.setHgap(10);
+		pane = new BorderPane();
+		Scene scene = new Scene (pane, 500,500);
+		GridPane grid = new GridPane();
+		grid.setVgap(10);
+		grid.setHgap(10);
 		//pane.setPadding(new Insets (10,10,10,10));
 		
 		Label copyright = new Label ("  copyright The Team\n");
@@ -33,14 +45,48 @@ public class TowerDefenseGUI extends Application{
 		Button instructions = new Button("Instructions");
 		instructions.setMinWidth(120);
 		
-		pane.add(newGame, 7, 17);
-		pane.add(loadGame, 7, 18);
-		pane.add(instructions, 7, 19);
-		pane.add(copyright, 0, 39);
+		grid.add(newGame, 7, 17);
+		grid.add(loadGame, 7, 18);
+		grid.add(instructions, 7, 19);
+		grid.add(copyright, 0, 39);
+		ButtonHandler handler = new ButtonHandler();
+		newGame.setOnAction(handler);
+		
+		gameView = new GameView(theGame);
+		
 		//pane.set
-		window.setCenter(pane);
+		pane.setCenter(grid);
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	private class ButtonHandler implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+			// TODO Auto-generated method stub
+			
+			Button clicked = (Button) event.getSource();
+			
+			if (clicked.getText().equals("New Game")) {
+				setViewTo(gameView);
+				System.out.println("Game View");
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	/*
+	   * void setViewTo(Observer)
+	   * sets the current view to be the input observer  
+	   */
+	  private void setViewTo(Observer newView) {
+		    pane.setCenter(null); // set the center of pane to null
+		    currentView = newView; // update the current view to the input observer
+		    pane.setCenter((Node) currentView); // set the center of the pane to the current observer
+		  }
 
 }
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,8 +1,44 @@
 package view;
 
-public class GameView {
+import java.util.Observable;
+import java.util.Observer;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import model.Map;
+
+public class GameView extends BorderPane implements Observer{
  /*
   * Default GameUI on some side of the screen,
   * Game view / Map occupying everywhere else.
   */
+	private Map theGame;
+	private final Image background = new Image("file:images/map_1.jpg");
+	private Canvas canvas;
+	private GraphicsContext gc;
+	
+	final int canvasL = 900; // canvasL will be the size of the canvas/gc
+	
+	public GameView (Map map) {
+		
+		theGame = map; // copy the input game from the input to theGame
+		canvas = new Canvas (canvasL, canvasL); // Initialize the canvas with canvasLxcanvasL
+		gc = canvas.getGraphicsContext2D(); 
+		
+		drawMap();
+		this.setCenter(canvas); // set the center of this boarderpane to be the canvas
+	}
+
+	private void drawMap() {
+		gc.drawImage(background, 0, 0);
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
