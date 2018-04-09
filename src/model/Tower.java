1a583f9a2531f020850f88dd8401b14f1bb1d8c1
Devon Oberdan <devonoberdan@gmail.com>
2018-04-08 19:51:18 -0700

    Abstract tower class, first tower constructor

--- src/model/Tower.java
+++ src/model/Tower.java
@@ -1,5 +1,27 @@
 package model;
 
-public class Tower {
+import javafx.scene.image.Image;
+import javafx.scene.media.Media;
 
+public abstract class Tower {
+	protected float    range;
+	protected int      damage;
+	protected int      xp;
+	protected int      currentLevel;
+	protected int[]    upgradeCosts;
+	
+	protected Image[]  images;
+	protected Image[]  projectiles;
+	protected Media    soundEffect;
+	
+	public float  getRange()             { return range;        }
+	public int    getDamage()            { return damage;       }
+	public int    getXP()                { return xp;           }
+	public int    getLevel()             { return currentLevel; }
+	public int    getCost()              { return upgradeCosts[currentLevel]; }
+	
+	public Image  getCurrentImage()      { return images[currentLevel];      }
+	public Image  getCurrentProjectile() { return projectiles[currentLevel]; }
+	public Media  getSoundEffect()       { return soundEffect;               }
+	
 }
