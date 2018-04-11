682bf18fcbc98187e6454302d304c5bc551c3106
Badr Badghaish <badrb@email.arizona.edu>
2018-04-10 19:48:59 -0700

    bb

--- src/model/NewEnemy.java
+++ src/model/NewEnemy.java
@@ -10,10 +10,11 @@ public class NewEnemy {
 
 	private Point loc;
 	private int hel;
-	private final Image img;
+	private final Image img_n, img_d;
 	private int speed;
 	private Path path;
 	private Point turns;
+	private boolean attacked;
 
 	public NewEnemy(Point offset, int speed, Path path) {
 		loc = new Point(-60 -(int) offset.getX(), 17 -(int) offset.getY());
@@ -21,7 +22,9 @@ public class NewEnemy {
 		this.speed = speed;
 		this.path = path;
 		turns = new Point(1,1);
-		img = new Image("file:images/enemy_sprite.png");
+		img_n = new Image("file:images/enemy_sprite.png");
+		img_d = new Image("file:images/enemy_sprite_damge.png");
+		attacked = false;
 	}
 	
 	public void setLoc(Point p) { loc = p; }
@@ -35,16 +38,16 @@ public class NewEnemy {
 			return;
 		switch (num) {
 		case 0:
-			gc.drawImage(img, 0, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
+			gc.drawImage(img_n, 0, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
 			break;
 		case 1:
-			gc.drawImage(img, 60, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
+			gc.drawImage(img_n, 60, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
 			break;
 		case 2:
-			gc.drawImage(img, 120, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
+			gc.drawImage(img_n, 120, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
 			break;
 		default:
-				gc.drawImage(img, 180, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
+				gc.drawImage(img_n, 180, 0, 60, 60, loc.getX(), loc.getY(), 60, 60);
 				break;
 		}
 		checkTurns();
@@ -57,4 +60,5 @@ public class NewEnemy {
 	private void checkTurns() {
 		turns = path.checkTurns(loc);
 	}
+	
 }
